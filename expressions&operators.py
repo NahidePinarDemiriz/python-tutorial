d202e992452c51f2cea0c45fef807e209c2fdf65
NahidePınarDemiriz <demiriznahidepinar@gmail.com>
2024-09-09 15:21:01 +0300

    Modified expressions&operators.py file.

---
+++ expressions&operators.py
@@ -0,0 +1,61 @@
+# Expressions
+# + operator
+print(2+2)
+print(type(2+2))
+
+print(2.4+2.3)
+print(type(2.4+2.3))
+
+print(2.4+2)
+print(type(2.4+2))
+
+# - operator
+print(4-2)
+print(type(4-2))
+
+print(2.4-2.1)
+print(type(2.4-2.1))
+
+# * operator
+print(4*2)
+print(type(4*2))
+
+print(2.4*2.1)
+print(type(2.4*2.1))
+
+print(2.4*2)
+print(type(2.4*2))
+
+# / operator
+print(3/2)
+print(type(3/2))
+
+print(3/1.5)
+print(type(3/1.5))
+
+# // int division operator
+print(5//2)
+print(type(5//2))
+
+print(5//2.5)
+print(type(5//2.5))
+
+print(4.2//5)
+print(type(4.2//5))
+
+# % mod operator
+print(5%2)
+print(type(5%2))
+
+print(2.5%2)
+print(type(2.5%2))
+
+# ** mod operator
+print(2**3)
+print(type(2**3))
+
+print(2.4**2)
+print(type(2.4**2))
+
+print(5**-1)
+print(type(5**-1))
